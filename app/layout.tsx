2dd439e9ef9b3bd368a5347daeb6bbddead30d3a
voetbalkitcom-cloud <voetbalkit.com@gmail.com>
2026-04-23 12:39:22 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="nl">
+    <html lang="nl" data-theme="light">
       <body>
         <SiteHeader />
         {children}
